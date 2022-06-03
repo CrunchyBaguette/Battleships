31a3d0a25903c11066182d354b6b3d2ee0b4a2b9
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2022-06-03 17:38:48 +0200

    Corrected server

--- server/index.js
+++ server/index.js
@@ -11,11 +11,45 @@ wss.on("connection", ws => {
         ws.terminate();
     } else {
         ws.id = playerIDs.pop();
+        ws.ready = false;
         ws.send("My ID = "+ws.id.toString());
 
         ws.on("close", () => {
             console.log("Client has disconnected!");
             playerIDs.push(ws.id);
-        })
+        });
+
+        ws.addEventListener("message", e => {
+            // console.log([...wss.clients].indexOf(ws));
+            switch(e.data){
+                case "connect":
+                    if(ws.ready){
+                        ws.ready = false;
+                    } else {
+                        ws.ready = true;
+                    }
+                    var count = 0;
+                    for(let i=0; i<wss.clients.size; i++){
+                        if([...wss.clients][i].ready){
+                            count += 1;
+                        }
+                    }
+        
+                    if(count == 2){
+                        console.log("Let's start!");
+                        [...wss.clients][0].send("sendBoard");
+                        [...wss.clients][1].send("sendBoard");
+                    }
+                    break;
+                default:
+                    console.log("ship info of player "+ws.id.toString());
+                    console.log(e.data);
+                    break;
+            }
+        });
+
+        let notify = function(ws1, ws2) {
+            
+        }
     }
 });
